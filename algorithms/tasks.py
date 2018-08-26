5e336accccddb4a29a08ec3e66640755165cd281
1 <ppark9553@gmail.com>
2018-08-26 21:50:44 +0900

    원티드 페이지 마지막 데이터 정리 완료

--- algorithms/tasks.py
+++ algorithms/tasks.py
@@ -28,23 +28,69 @@ def word_pos_tag_task(data):
     return result
 
 @shared_task
-def process_wanted_data():
+def view_all_wanted_data_structures():
     w = WantedProcessor()
-    title_list, company_dict, tech_list, url_dict = w.wanted_model()
-    final_sorted_list = w.refine_data(tech_list)
-    top_skill = w.create_topskill_list(final_sorted_list, tech_list)
-    wantedjob_list = w.create_wantedjob_list(final_sorted_list, company_dict)
-    skill_count = w.create_skill_category_count(title_list)
-
-    print(url_dict)
-    print(top_skill)
-    print(wantedjob_list)
-    print(skill_count)
-
-    w.save_data_to_db('COMPANY_URLS', url_dict)
-    w.save_data_to_db('TOP_SKILLS', top_skill)
-    w.save_data_to_db('WANTED_SKILL_COMPS', wantedjob_list)
-    w.save_data_to_db('SKILL_COUNTS', skill_count)
+    wanted_content_model = w.get_wanted_model_data()
+    # DATA 1
+    hire_title_list = w.create_hire_title_list(wanted_content_model)
+    print('1 - hire_title_list')
+    print(hire_title_list)
+    print('======================')
+    # DATA 2
+    tech_list = w.create_tech_list(wanted_content_model)
+    print('2 - tech_list')
+    print(tech_list)
+    print('======================')
+    # DATA 3
+    company_tech_dict = w.create_company_tech_dict(wanted_content_model)
+    print('3 - company_tech_dict')
+    print(company_tech_dict)
+    print('======================')
+    # DATA 4
+    company_hire_url_dict = w.create_company_hire_url_dict(wanted_content_model)
+    print('4 - company_hire_url_dict')
+    print(company_hire_url_dict)
+    print('======================')
+    # DATA 5
+    skill_category_count = w.create_skill_category_count(hire_title_list)
+    print('5 - skill_category_count')
+    print(skill_category_count)
+    print('======================')
+    # DATA 5.5
+    highcharts_skill_category_count = w.create_highcharts_skill_category_count(skill_category_count)
+    print('5.5 - highcharts_skill_category_count')
+    print(highcharts_skill_category_count)
+    print('======================')
+    # DATA 6
+    sorted_skill_hire_count_list = w.create_sorted_skill_hire_count_list(tech_list)
+    print('6 - sorted_skill_hire_count_list')
+    print(sorted_skill_hire_count_list)
+    print('======================')
+    # DATA 7
+    clean_sorted_top_200_skill_hire_count_list = w.create_clean_sorted_top_200_skill_hire_count_list(tech_list)
+    print('7 - clean_sorted_top_200_skill_hire_count_list')
+    print(clean_sorted_top_200_skill_hire_count_list)
+    print('======================')
+    # DATA 8
+    topskill_highcharts_list = w.create_topskill_highcharts_list(clean_sorted_top_200_skill_hire_count_list)
+    print('8 - topskill_highcharts_list')
+    print(topskill_highcharts_list)
+    print('======================')
+    # DATA 9
+    full_wantedjob_list = w.create_full_wantedjob_list(clean_sorted_top_200_skill_hire_count_list, company_tech_dict)
+    print('9 - full_wantedjob_list')
+    print(full_wantedjob_list)
+    print('======================')
+    # DATA 10
+    wantedjob_table_list = w.create_wantedjob_table_list(clean_sorted_top_200_skill_hire_count_list, company_tech_dict)
+    print('10 - wantedjob_table_list')
+    print(wantedjob_table_list)
+    print('======================')
+    # DATA 11
+    category_skill_hire_count_highcharts_data = w.create_category_skill_hire_count_highcharts_data(full_wantedjob_list)
+    print('11 - category_skill_hire_count_highcharts_data')
+    print(category_skill_hire_count_highcharts_data)
+    print('======================')
 
 @shared_task
 def make_wanted_data_and_cache():
